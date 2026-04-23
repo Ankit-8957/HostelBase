b75b40909bfde3e8cfe75751cb1f91421e51b88e
Ankit Pandey <apandey8957@gmail.com>
2026-04-23 22:00:49 +0530

    add backend

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hostelbase-backend.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // 🔥 ALWAYS send cookies
 });
 api.interceptors.response.use(
